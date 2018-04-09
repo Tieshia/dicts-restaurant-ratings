c5b06cad4064a55e205e8cd055c97351d2f145b2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 16:08:48 -0700

    Validated user input. and unpacked sorted_ratings tuple.

--- ratings.py
+++ ratings.py
@@ -1,37 +1,44 @@
 import sys
 
+
 def create_restaurant_ratings(filename=sys.argv[1]):
     """Restaurant rating lister."""
 
     restaurant_ratings = {}  # initialize restaurant_ratings dictionary
 
-    with open(filename) as restaurant_file: # import the file
-        for line in restaurant_file: # for each line:
+    with open(filename) as restaurant_file:  # import the file
+        for line in restaurant_file:  # for each line:
             line = line.rstrip()  # strip right whitespace
             restaurant, rating = line.split(':')  # split by ':'
             restaurant_ratings[restaurant] = int(rating)  # add list elements to dictionary
 
     return restaurant_ratings
 
+
 def print_sorted_ratings(restaurant_ratings):
     # create sorted list of items within restaurant_ratings dictionary
     sorted_ratings = sorted(restaurant_ratings.items())
 
-    print "\n"
-    
+    print
+
     # print restaurant and rating statements
-    for record in sorted_ratings:
-        restaurant, rating = record
+    for restaurant, rating in sorted_ratings:
         print "%s is rated at %d." % (restaurant, rating)
+
 
 def input_new_restaurant_rating(restaurant_ratings):
     # ask user for new restaurant and rating
     restaurant = raw_input('Enter a new restaurant name: ')
-    rating = raw_input('Enter its rating: ')    
+    rating = None
+
+    while rating is None or rating < 1 or rating > 5:
+        rating = int(raw_input('Rate the resturant between 1 and 5: '))
+
     # add new restaurant and rating to restaurant_ratings
-    restaurant_ratings[restaurant] = int(rating)
+    restaurant_ratings[restaurant] = rating
     # print new sorted list of restaurant_ratings
     return restaurant_ratings
+
 
 def print_restaurant_ratings():
     restaurant_ratings = create_restaurant_ratings()
