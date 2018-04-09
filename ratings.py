6be0da07bbfb28688bb16745c5ade223e6c0d0c1
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 15:27:02 -0700

    Sorted ratings.

--- ratings.py
+++ ratings.py
@@ -1,6 +1,5 @@
 import sys
 
-# filename=
 
 def print_sorted_ratings(filename=sys.argv[1]):
     """Restaurant rating lister."""
@@ -12,9 +11,11 @@ def print_sorted_ratings(filename=sys.argv[1]):
             line = line.rstrip()  # strip right whitespace
             restaurant, rating = line.split(':')  # split by ':'
             restaurant_ratings[restaurant] = rating  # add list elements to dictionary
-    print restaurant_ratings
-
-print_sorted_ratings()
 
     # create sorted list of items within restaurant_ratings dictionary
+    sorted_ratings = sorted(restaurant_ratings.items())
+    print sorted_ratings
     # print restaurant and rating statements
+
+
+print_sorted_ratings()
